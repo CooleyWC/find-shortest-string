d73f1538bcbc5900318ab9aeee789245a5083da3
Will Cooley <william.cooley@gmail.com>
2024-08-01 19:18:31 -0400

    complete lab

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,11 @@
 function findShortestString(arr) {
-  // type your code here
+  let shortest = arr[0]
+  for(let i=1; i<arr.length; i++){
+    if(arr[i].length<shortest.length){
+      shortest = arr[i]
+    }
+  }
+  return shortest
 }
 
 if (require.main === module) {
@@ -23,4 +29,11 @@ if (require.main === module) {
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
+// set an shortest element variable to empty
+// for loop the array
+// for each element, check it's length
+// if the elements length is shorter than the shortest element variable, set the variable
+// if the element is same length as variable, do not reset it
+// return the variable
+
 // And a written explanation of your solution
